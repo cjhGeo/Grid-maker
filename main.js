870b9070cdf6e8bf9783b6de3fc119cd607ef3dd
cjhGeo <107737553+cjhGeo@users.noreply.github.com>
2023-04-28 18:01:38 +0800

    Added Features (mostly cross)
    Added function to check hex code validity
    Added function to add/remove crosses in grids and stuff

--- main.js
+++ main.js
@@ -4,14 +4,17 @@ let loadFile = function (event) {
 }
 
 function loadGrid() {
-    document.querySelector("#gridCell").style.borderColor = String(document.querySelector("#colour").value);
-    document.querySelector("#gridCell").style.borderWidth = String(document.querySelector("#gridWidth").value) + "px";
+    document.querySelector("#grid").innerHTML = '<div class="gridCell" id="gridCell1" onclick="markSpot(this.id)"></div>';
+
+    document.querySelector(".gridCell").style.borderColor = String(document.querySelector("#colour").value);
+    document.querySelector(".gridCell").style.borderWidth = String(document.querySelector("#gridWidth").value) + "px";
 
     let cellNo = (document.querySelector("#col").value * document.querySelector("#row").value) - 1;
     for (let i = 0; i < cellNo; i++) {
         var anchor = document.querySelector("#grid");
-        var newType = document.querySelector("#gridCell").cloneNode(true)
-        newType.id = "gridCell";
+        var newType = document.querySelector(".gridCell").cloneNode(true)
+        newType.class = "gridCell";
+        newType.id = "gridCell" + String(i + 2);
         anchor.appendChild(newType);
     }
 
@@ -20,4 +23,30 @@ function loadGrid() {
 
     document.querySelector("#grid").style.height = String(document.querySelector("img").naturalHeight) + "px";
     document.querySelector("#grid").style.width = String(document.querySelector("img").naturalWidth) + "px";
+}
+
+function markSpot(gridCell) {
+    if (document.querySelector("#" + String(gridCell)).innerHTML == "") {
+        document.querySelector("#" + String(gridCell)).innerHTML = '<i class="fa-sharp fa-regular fa-xmark" style="outline: none; color: ' + String(document.querySelector("#markerColour").value) + ';"></i>';
+    } else {
+        document.querySelector("#" + String(gridCell)).innerHTML = ""
+    }
+
+    let gridCellWidth = ((document.querySelector("img").naturalWidth) / (document.querySelector("#col").value));
+    let gridCellHeight = ((document.querySelector("img").naturalHeight) / (document.querySelector("#row").value));
+
+    document.querySelectorAll("i").forEach(i => {
+        i.style.fontSize = (gridCellWidth < gridCellHeight ? (String(gridCellWidth) + "px") : (String(gridCellHeight) + "px"));
+    });
+}
+
+function checkHex(hexOf) {
+    document.querySelector("#errorDiv").innerHTML = "The Hex Code(s) is/are not valid!";
+    let fullHex = /^#[a-zA-Z0-9]{6}$/;
+    let halfHex = /^#[a-zA-Z0-9]{3}$/;
+    console.log((document.querySelector("#" + String(hexOf)).value.match(fullHex)));
+    if (document.querySelector("#" + String(hexOf)).value.match(fullHex) || document.querySelector("#" + String(hexOf)).value.match(halfHex) || document.querySelector("#" + String(hexOf)).value == "") {
+        document.querySelector("#errorDiv").innerHTML = "";
+        console.log("Invalid!");
+    }
 }
